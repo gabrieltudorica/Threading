e8a5e9d91cd34f3b477acdb4bad2cc98bc6fdca8
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-03-05 17:01:31 +0200

    Refactored

--- Threads/StepFour/MaximumNumber.cs
+++ Threads/StepFour/MaximumNumber.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,144 +8,88 @@ namespace StepFour
     public class MaximumNumber
     {
         private readonly int threadPoolSize;
-        private readonly int partitionSize;
 
         private readonly List<Task<int>> activeTasks;
-        private readonly ConcurrentQueue<List<int>> partitions;
         private ConcurrentQueue<int> results;
+        private readonly Partitions partitions;
 
         public MaximumNumber(int threadPoolSize, int partitionSize)
         {
             this.threadPoolSize = threadPoolSize;
-            this.partitionSize = partitionSize;
 
             activeTasks = new List<Task<int>>();
-            partitions = new ConcurrentQueue<List<int>>();
             results = new ConcurrentQueue<int>();
+            partitions = new Partitions(partitionSize);
+        }
 
+        private void Setup(List<int> partition)
+        {
+            partitions.Generate(partition);            
+            CreateTasks();
+            activeTasks.ForEach(task=>task.Start());
         }
 
         public int GetFrom(List<int> initialCollection)
         {
-            CreatePartitions(initialCollection);
-            CreateTasks();
-            StartTasks();
+            Setup(initialCollection);
 
             while (true)
             {
-                int index = Task.WaitAny(activeTasks.ToArray());
-                var task = activeTasks[index];
-                var result = task.Result;
-                if(result != 0)
-                {
-                    results.Enqueue(task.Result);
+                Task<int> task = GetCompletedTask();
+                results.Enqueue(task.Result);
+                activeTasks.Remove(task);
+
+                if (partitions.AnyLeft())
+                {             
+                    ReplaceTask();
                 }                
 
-                if(partitions.Count > 0)
+                if (IsFinished())
                 {
-                    activeTasks.Remove(task);
-                    task = GetTask();
-                    activeTasks.Add(task);
-                    task.Start();
-                }
-                else
-                {
-                    activeTasks.Remove(task);
-                }
-
-                if (activeTasks.Count == 0 && results.Count == 1)
-                {
-                    return GetNextElement();
+                    return results.Single();
                 }
 
                 if (activeTasks.Count == 0)
-                {                    
-                    RestartWithParition(results.ToList());
+                {
+                    Setup(results.ToList());
+                    results = new ConcurrentQueue<int>();                    
                 }
             }
         }
 
-        private void CreatePartitions(List<int> initialCollection)
+        private Task<int> GetCompletedTask()
         {
-            var partitioner = Partitioner.Create(0, initialCollection.Count, partitionSize);
-
-            foreach (Tuple<int, int> range in partitioner.GetDynamicPartitions())
-            {
-                CreatePartition(initialCollection, range);
-            }
+            int index = Task.WaitAny(activeTasks.ToArray());
+            var task = activeTasks[index];
+            return task;
         }
 
-        private void CreatePartition(List<int> initialCollection, Tuple<int, int> range)
+        private bool IsFinished()
         {
-            var partition = new List<int>();
-
-            for (int i = range.Item1; i < range.Item2; i++)
-            {
-                partition.Add(initialCollection[i]);
-            }
-
-            partitions.Enqueue(partition);
+            return activeTasks.Count == 0 && results.Count == 1;
         }
 
+        private void ReplaceTask()
+        {
+            Task<int> task = GetTask(partitions.GetNext());
+            activeTasks.Add(task);
+            task.Start();
+        }
+       
         private void CreateTasks()
         {
             for (int i = 0; i < threadPoolSize; i++)
             {
-                activeTasks.Add(GetTask());
+                if (partitions.AnyLeft())
+                {
+                    activeTasks.Add(GetTask(partitions.GetNext()));
+                }
             }
         }
 
-        private Task<int> GetTask()
+        private Task<int> GetTask(List<int> partition)
         {
-            return new Task<int>(FindMaximum);
-        }
-
-        private int FindMaximum()
-        {
-            List<int> partition = GetNextPartition();
-
-            if (partition.Count > 0)
-            {
-               return partition.Max();
-            }
-
-            return 0;
-        }
-
-        private List<int> GetNextPartition()
-        {
-            List<int> partition;
-            if (!partitions.TryDequeue(out partition))
-            {
-                partition = new List<int>();
-            }
-
-            return partition;
-        }
-
-        private void StartTasks()
-        {
-            foreach (Task<int> task in activeTasks)
-            {
-                task.Start();
-            }
-        }
-
-        private int GetNextElement()
-        {
-            int currentValue;
-            results.TryDequeue(out currentValue);
-
-            return currentValue;
-        }
-
-        private void  RestartWithParition(List<int> partition)
-        {
-            CreatePartitions(partition);
-            results = new ConcurrentQueue<int>();
-            
-            CreateTasks();
-            StartTasks();
+            return new Task<int>(partition.Max);
         }
     }
 }
--- Threads/StepThree/MaximumNumber.cs
+++ Threads/StepThree/MaximumNumber.cs
@@ -24,6 +24,7 @@ namespace StepThree
             CreateInitialCollection(initialCollection);
             StartTasks();
 
+            //todo: remove while
             while (true)
             {
                 if (numbers.Count == 1)
